0c658173df602bef7691cd79a541241d89a66f17
Zia Ahmed <ziaahmedf17@gmail.com>
2025-08-21 16:30:14 +0500

    Initial Commit

---
+++ Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Zi on 21/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hot ProspectsTests/Hot_ProspectsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsTests.swift
+//  Hot ProspectsTests
+//
+//  Created by Zi on 21/08/2025.
+//
+
+import Testing
+@testable import Hot_Prospects
+
+struct Hot_ProspectsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
